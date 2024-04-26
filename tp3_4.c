e46159e8ab253976c1f6cf5a06565f4b2e8e9603
ElBranda <martinisaiasbrandalisijimenez@gmail.com>
2024-04-26 08:23:55 -0300

    tp3_4

---
+++ tp3_4.c
@@ -0,0 +1,16 @@
+char *TiposProductos[]={"Galletas","Snack","Cigarrillos","Caramelos","Bebidas"};
+
+typedef struct {
+    int ProductoID; //Numerado en ciclo iterativo
+    int Cantidad; // entre 1 y 10
+    char *TipoProducto; // Algún valor del arreglo TiposProductos
+    float PrecioUnitario; // entre 10 - 100
+} Producto;
+
+typedef struct {
+    int ClienteID; // Numerado en el ciclo iterativo
+    char *NombreCliente; // Ingresado por usuario
+    int CantidadProductosAPedir; // (aleatorio entre 1 y 5)
+    Producto *Productos //El tamaño de este arreglo depende de la variable
+    // “CantidadProductosAPedir”
+} Cliente;
